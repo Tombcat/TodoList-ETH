f9a580fe8aac63479facaedb21d3f3938f7028f5
Tombcat <marcin.wlodarczyk98@gmail.com>
2021-07-07 14:32:59 +0200

    2. List Tasks

---
+++ test/TodoList.test.js
@@ -0,0 +1,26 @@
+const { assert } = require("chai")
+
+const TodoList = artifacts.require('./TodoList.sol')
+
+contract('TodoList', (accounts)=>{
+    before(async () =>{
+        this.TodoList = await TodoList.deployed()
+    })
+
+    it('Deploys successfully', async()=>{
+        const address = await this.TodoList.address
+        assert.notEqual(address, 0x0)
+        assert.notEqual(address, '')
+        assert.notEqual(address, null)
+        assert.notEqual(address, undefined)
+    })
+
+    it('Lists tasks', async ()=>{
+        const taskCount = await this.TodoList.taskCount()
+        const task = await this.TodoList.tasks(taskCount)
+        assert.equal(task.id.toNumber(), taskCount.toNumber())
+        assert.equal(task.content, 'Check out dappuniversity.com')
+        assert.equal(task.completed, false)
+        assert.equal(taskCount.toNumber(), 1)
+    })
+})
